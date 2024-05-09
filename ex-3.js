c9f66f2f3669fae99065d9558837ecd599793b2c
Krittayapornn <krittayaporn.ww@gmail.com>
2024-05-09 13:51:38 +0700

    feat : values variables JS

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,7 @@
 // Exercise #3: Celsius to Fahrenheit
 let celsius = 30;
 
-// Start coding here;
+let celsiusToFahrenheit = (celsius * 1.8)+32;
 
-let fahrenheit;
+let fahrenheit = celsiusToFahrenheit;
+console.log (fahrenheit)
